419300743f749d5b09ab510a2673b491c31a6a5a
LYN <200800805@qq.com>
2025-05-14 16:17:17 +0800

    打包文件

---
+++ client_package/dist/sync_mods.bat
@@ -0,0 +1,2 @@
+@echo off
+ModSyncClient.exe --server http://服务器IP:5000 --mods-dir "%APPDATA%\.minecraft\mods" --password your_secure_password_here --sync
---
+++ server_package/dist/start_server.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+./ModSyncServer \
+    --host 0.0.0.0 \
+    --port 5000 \
+    --mods-dir ./server_mods \
+    --password your_secure_password_here
